9c7796ae17afe481806a430fec9f279878060173
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2023-10-17 15:31:49 -0300

    updt

--- fractalplatonic/vr/nomes.js
+++ fractalplatonic/vr/nomes.js
@@ -74,3 +74,44 @@ function mnivel() {
 	  entityEl4.setAttribute('visible','true');
 	}
 }
+
+function mnivelRA() {
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl0 = sceneEl.querySelector('.nivel0')
+	var entityEl1 = sceneEl.querySelector('.nivel1')
+	var entityEl2 = sceneEl.querySelector('.nivel2')
+	var entityEl3 = sceneEl.querySelector('.nivel3')
+	var entityEl4 = sceneEl.querySelector('.nivel4')
+	var p = document.getElementById("nv").value
+	if (p == 0) { 
+	  entityEl0.setAttribute('visible','true');
+	  entityEl1.setAttribute('visible','false');
+	  entityEl2.setAttribute('visible','false');
+	  entityEl3.setAttribute('visible','false');
+	  entityEl4.setAttribute('visible','false');
+	} else if (p == 1) {
+	  entityEl0.setAttribute('visible','false');
+	  entityEl1.setAttribute('visible','true');
+	  entityEl2.setAttribute('visible','false');
+	  entityEl3.setAttribute('visible','false');
+	  entityEl4.setAttribute('visible','false');
+	} else if (p == 2) {
+	  entityEl0.setAttribute('visible','false');
+	  entityEl1.setAttribute('visible','false');
+	  entityEl2.setAttribute('visible','true');
+	  entityEl3.setAttribute('visible','false');
+	  entityEl4.setAttribute('visible','false');
+	} else if (p == 3) {
+	  entityEl0.setAttribute('visible','false');
+	  entityEl1.setAttribute('visible','false');
+	  entityEl2.setAttribute('visible','false');
+	  entityEl3.setAttribute('visible','true');
+	  entityEl4.setAttribute('visible','false');
+	} else {
+	  entityEl0.setAttribute('visible','false');
+	  entityEl1.setAttribute('visible','false');
+	  entityEl2.setAttribute('visible','false');
+	  entityEl3.setAttribute('visible','false');
+	  entityEl4.setAttribute('visible','true');
+	}
+}
