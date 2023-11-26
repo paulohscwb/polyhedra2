5a75897d21312604075ffc935c39ff4c8d443edc
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2023-11-25 23:19:03 -0300

    update

--- fractalplatonic/vr/nomes.js
+++ fractalplatonic/vr/nomes.js
@@ -9,48 +9,375 @@ function sairVR() {
 	}
 }
 
-function fracs() {
+function fracCubo() {
 	var sceneEl = document.querySelector('a-scene')
 	var ent = document.getElementById("cena")
 	while (ent.firstChild) {
         ent.removeChild(ent.firstChild)
     }
-	sc = 6
-	var box = document.createElement("a-tetrahedron")
-	  box.setAttribute("mixin", "mtl1")
-	  box.setAttribute("scale", `${sc} ${sc} ${sc}`)
-	  if (i == nv.value) {
-		  ent.appendChild(box)
-	  }
-	  
-	for (var i = 1; i <= nv.value; i++) {
-	  var box = document.createElement("a-tetrahedron")
-	  box.setAttribute("mixin", "mtl1 p1")
-	  box.setAttribute("scale", `${sc} ${sc} ${sc}`)
-	  if (i == nv.value) {
-		  ent.appendChild(box)
-	  }
-	  var box = document.createElement("a-tetrahedron")
-	  box.setAttribute("mixin", "mtl1 p2")
-	  box.setAttribute("scale", `${sc} ${sc} ${sc}`)
-	  if (i == nv.value) {
-		  ent.appendChild(box)
-	  }
-	  var box = document.createElement("a-tetrahedron")
-	  box.setAttribute("mixin", "mtl1 p3")
-	  box.setAttribute("scale", `${sc} ${sc} ${sc}`)
-	  if (i == nv.value) {
-		  ent.appendChild(box)
-	  }
-	  var box = document.createElement("a-tetrahedron")
-	  box.setAttribute("mixin", "mtl1 p4")
-	  box.setAttribute("scale", `${sc} ${sc} ${sc}`)
-	  if (i == nv.value) {
-		  ent.appendChild(box)
-	  }
-	  sc *= 0.5
+	sc = 6;
+	cd = 1;
+	fat = 3;
+	coord0 = cd*fat	
+	
+	var modulo = (a, b, c) => {
+		ent2.setAttribute("position", `${a} ${b} ${c}`)
+		for ( i = 1; i <= 20; i++) {
+			var box = document.createElement("a-box")
+			box.setAttribute("mixin", `mtl2 p${i}d`)
+			ent2.appendChild(box)
+		}
 	}
+		
+	var moduloM = (a, b, c) => {
+		ent3.setAttribute("position", `${a} ${b} ${c}`)
+		vv = ""
+		var items = (a) => {
+			for (i = 1; i <= 20; i++) { 
+				var box = document.createElement("a-box")
+				box.setAttribute("mixin", `mtl2 p${i}d`)
+				ent2.appendChild(box)
+			}
+		}
+		
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} ${coord0} 0`)
+			items(1);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} ${coord0} 0`)
+			items(2);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} ${-coord0} 0`)
+			items(3);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} ${-coord0} 0`)
+			items(4);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `0 ${coord0} ${coord0}`)
+			items(5);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `0 ${coord0} ${-coord0}`)
+			items(6);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `0 ${-coord0} ${coord0}`)
+			items(7);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `0 ${-coord0} ${-coord0}`)
+			items(8);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} ${coord0} ${coord0}`)
+			items(9);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} ${coord0} ${-coord0}`)
+			items(10);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} ${-coord0} ${coord0}`)
+			items(11);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} ${-coord0} ${-coord0}`)
+			items(12);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} ${coord0} ${coord0}`)
+			items(13);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} ${coord0} ${-coord0}`)
+			items(14);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} ${-coord0} ${coord0}`)
+			items(15);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} ${-coord0} ${-coord0}`)
+			items(16);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} 0 ${coord0}`)
+			items(17);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${coord0} 0 ${-coord0}`)
+			items(18);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} 0 ${coord0}`)
+			items(19);
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-coord0} 0 ${-coord0}`)
+			items(20);
+		ent3.appendChild(ent2)
+	}
+	
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		ent1.setAttribute("id", "nivel0")
+		box = document.createElement("a-box")
+		box.setAttribute("mixin", "mtl1")
+		ent1.appendChild(box)
+	ent.appendChild(ent1)
+		sc /= 3;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		ent1.setAttribute("id", "nivel1")
+		ent1.setAttribute("visible", "false")
+		var ent2 = document.createElement("a-entity")
+		modulo(0, 0, 0);
+		ent1.appendChild(ent2)
+	ent.appendChild(ent1)
+		sc /= 3;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		ent1.setAttribute("id", "nivel2")
+		ent1.setAttribute("visible", "false")
+		var ent3 = document.createElement("a-entity")
+		moduloM(0, 0, 0);
+		ent1.appendChild(ent3);
+	ent.appendChild(ent1)
+		sc /= 3;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		ent1.setAttribute("id", "nivel3")
+		ent1.setAttribute("visible", "false")
+		coord1 = cd*fat*3
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, coord1, 0);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, coord1, 0);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, -coord1, 0);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, -coord1, 0);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(0, coord1, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(0, coord1, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(0, -coord1, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(0, -coord1, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, coord1, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, coord1, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, -coord1, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, -coord1, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, coord1, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, coord1, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, -coord1, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, -coord1, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, 0, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(coord1, 0, -coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, 0, coord1);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-coord1, 0, -coord1);
+		ent1.appendChild(ent3);
+	ent.appendChild(ent1)
+	
+	var sceneEls = document.querySelectorAll('a-box')
+	sceneEls.setAttribute("mixin", `mtl2 p${i}d`)
 }
+
+function fracTetraedro() {
+	var sceneEl = document.querySelector('a-scene')
+	var ent = document.getElementById("cena")
+	while (ent.firstChild) {
+        ent.removeChild(ent.firstChild)
+    }
+	sc = 6;
+	cd = 0.57735;
+	fat = 2;
+	
+	var modulo = (a, b, c) => {
+		ent2.setAttribute("position", `${a} ${b} ${c}`)
+		for ( i = 1; i <= 4; i++) {
+			var box = document.createElement("a-tetrahedron")
+			box.setAttribute("mixin", `mtl2 p${i}`)
+			ent2.appendChild(box)
+		}
+	}
+	
+	var moduloM = (a, b, c) => {
+		ent3.setAttribute("position", `${a} ${b} ${c}`)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-cd*fat} ${cd*fat} ${-cd*fat}`)
+			for ( i = 1; i <= 4; i++) {
+				var box = document.createElement("a-tetrahedron")
+				box.setAttribute("mixin", `mtl2 p${i}`)
+				ent2.appendChild(box)
+			}
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${cd*fat} ${cd*fat} ${cd*fat}`)
+			for ( i = 1; i <= 4; i++) {
+				var box = document.createElement("a-tetrahedron")
+				box.setAttribute("mixin", `mtl2 p${i}`)
+				ent2.appendChild(box)
+			}
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${cd*fat} ${-cd*fat} ${-cd*fat}`)
+			for ( i = 1; i <= 4; i++) {
+				var box = document.createElement("a-tetrahedron")
+				box.setAttribute("mixin", `mtl2 p${i}`)
+				ent2.appendChild(box)
+			}
+		ent3.appendChild(ent2)
+		var ent2 = document.createElement("a-entity")
+			ent2.setAttribute("position", `${-cd*fat} ${-cd*fat} ${cd*fat}`)
+			for ( i = 1; i <= 4; i++) {
+				var box = document.createElement("a-tetrahedron")
+				box.setAttribute("mixin", `mtl2 p${i}`)
+				ent2.appendChild(box)
+			}
+		ent3.appendChild(ent2)
+	}
+	
+	if (nv.value == 0) {
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		var box = document.createElement("a-tetrahedron")
+		box.setAttribute("mixin", "mtl1")
+		ent1.appendChild(box)
+	} else if (nv.value == 1) {
+		sc /= 2;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		var ent2 = document.createElement("a-entity")
+		modulo(0, 0, 0);
+		ent1.appendChild(ent2)
+	} else if (nv.value == 2) {
+		sc /= 4;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		var ent3 = document.createElement("a-entity")
+		moduloM(0, 0, 0);
+		ent1.appendChild(ent3);
+	} else if (nv.value == 3) {
+		sc /= 8;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		var ent3 = document.createElement("a-entity")
+		moduloM(-cd*fat*2, cd*fat*2, -cd*fat*2);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(cd*fat*2, cd*fat*2, cd*fat*2);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(cd*fat*2, -cd*fat*2, -cd*fat*2);
+		ent1.appendChild(ent3);
+		var ent3 = document.createElement("a-entity")
+		moduloM(-cd*fat*2, -cd*fat*2, cd*fat*2);
+		ent1.appendChild(ent3);
+	} else if (nv.value == 4) {
+		sc /= 16;
+		var ent1 = document.createElement("a-entity")
+		ent1.setAttribute("scale", `${sc} ${sc} ${sc}`)
+		var ent4 = document.createElement("a-entity")
+		ent4.setAttribute("position", `${-cd*fat*4} ${cd*fat*4} ${-cd*fat*4}`)
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, -cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, -cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+		ent1.appendChild(ent4);
+		var ent4 = document.createElement("a-entity")
+		ent4.setAttribute("position", `${cd*fat*4} ${cd*fat*4} ${cd*fat*4}`)
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, -cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, -cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+		ent1.appendChild(ent4);
+		var ent4 = document.createElement("a-entity")
+		ent4.setAttribute("position", `${cd*fat*4} ${-cd*fat*4} ${-cd*fat*4}`)
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, -cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, -cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+		ent1.appendChild(ent4);
+		var ent4 = document.createElement("a-entity")
+		ent4.setAttribute("position", `${-cd*fat*4} ${-cd*fat*4} ${cd*fat*4}`)
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(-cd*fat*2, -cd*fat*2, cd*fat*2);
+			ent4.appendChild(ent3);
+			var ent3 = document.createElement("a-entity")
+			moduloM(cd*fat*2, -cd*fat*2, -cd*fat*2);
+			ent4.appendChild(ent3);
+		ent1.appendChild(ent4);
+	}
+	ent.appendChild(ent1)
+}
+
 
 function mfaces() {
 	var x = document.getElementById("faces")
